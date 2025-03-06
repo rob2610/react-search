4bd983d69a3e0571924f6189d85dad039a73ee3e
Roberto Volpe <roberto.volpe@farport.co>
2025-03-06 15:01:59 +0100

    created webapp for search images

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+    const [text, setText] = useState('')
+    const [result, setResult] = useState([])
+    const [nextPage, setNextPage] = useState(0)
+    const eventHandler = (event) => setText(event.target.value)
+    const clickHandler = async (click) => {
+        click.preventDefault()
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/images?query=${text}`, {method: 'GET'})
+        const json = await response.json()
+        console.log(json)
+        setResult(json.images)
+        setNextPage(json.nextPage)
+    }
+
+    return (
+        <>
+            <div>
+                <form className="form-container" onSubmit={clickHandler}>
+                    <input className="input-text" type="text" onChange={eventHandler} required/>
+                    <button className="submit-button" type="submit"> Invia</button>
+                    <button> Next Page: {nextPage}</button>
+                </form>
+
+                <div className="flex">
+                    {result.map((image, index) =>
+                        <div key={image.thumbnailUrl} className="flex-vertical">
+                           <a href={image.imageUrl} target="_blank"><img src={image.thumbnailUrl} style={{height: 150, width: 300}}/></a>
+                            <h4>{image.title}</h4>
+                        </div>)}
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default App
